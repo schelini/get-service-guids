b755636d0100b6196bfe0449fa761237360bff49
schelini <93011592+schelini@users.noreply.github.com>
2024-03-12 15:25:18 -0400

    Create get-service-guids.sh

---
+++ get-service-guids.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# get service plans as list
+SERVICE_OFFERING=$(cf curl '/v3/service_offerings?per_page=5000' | jq -r '.resources[] | select(.name=='\"$1\"').guid')
+SERVICE_PLANS=$(cf curl "/v3/service_plans?per_page=5000&service_offering_guids=$SERVICE_OFFERING" | jq -r '.resources[].guid' | sed -z 's/\n/,/g')
+
+# get service instance GUIDs
+cf curl "/v3/service_instances?per_page=5000&service_plan_guids=$SERVICE_PLANS" | jq -r '.resources[].guid'
