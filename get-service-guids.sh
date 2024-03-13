67c5e447aa32c2e241cb270d39b26f579fc2b631
schelini <93011592+schelini@users.noreply.github.com>
2024-03-13 10:19:35 -0400

    change sed to tr

--- get-service-guids.sh
+++ get-service-guids.sh
@@ -2,7 +2,7 @@
 
 # get service plans as list
 SERVICE_OFFERING=$(cf curl '/v3/service_offerings?per_page=5000' | jq -r '.resources[] | select(.name=='\"$1\"').guid')
-SERVICE_PLANS=$(cf curl "/v3/service_plans?per_page=5000&service_offering_guids=$SERVICE_OFFERING" | jq -r '.resources[].guid' | sed -z 's/\n/,/g')
+SERVICE_PLANS=$(cf curl "/v3/service_plans?per_page=5000&service_offering_guids=$SERVICE_OFFERING" | jq -r '.resources[].guid' | tr '\n' ',')
 
 # get service instance GUIDs
 cf curl "/v3/service_instances?per_page=5000&service_plan_guids=$SERVICE_PLANS" | jq -r '.resources[].guid'
